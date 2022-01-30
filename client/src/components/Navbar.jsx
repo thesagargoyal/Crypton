08d6ab910105a3b1bdfc27de13f647f15edb3ed5
Sagar Goyal <sagargoyal2015@gmail.com>
2022-01-30 16:12:13 +0530

    reformatted navbar and scrollbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,47 +13,10 @@ const NavBarItem = ({ title, classprops }) => (
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
-  const { connectWallet, currentAccount } = useContext(TransactionContext);
-
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    <nav className="w-full flex md:justify-center justify-center items-center p-4">
       <div className="flex flex-[0.5] justify-center items-center">
         <p className="text-white font-audio text-5xl">CRYPTON</p>
-      </div>
-      <div className="flex relative">
-        {!toggleMenu && (
-          <HiMenuAlt4
-            fontSize={28}
-            className="text-white md:hidden cursor-pointer"
-            onClick={() => setToggleMenu(true)}
-          />
-        )}
-        {toggleMenu && (
-          <AiOutlineClose
-            fontSize={28}
-            className="text-white md:hidden cursor-pointer"
-            onClick={() => setToggleMenu(false)}
-          />
-        )}
-        {toggleMenu && (
-          <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
-          >
-            <li className="text-xl w-full my-2">
-              <AiOutlineClose onClick={() => setToggleMenu(false)} />
-            </li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => (
-                <NavBarItem
-                  key={item + index}
-                  title={item}
-                  classprops="my-2 text-lg"
-                />
-              )
-            )}
-          </ul>
-        )}
       </div>
     </nav>
   );
